9d0677402cc08dde9688534dde430a4b973143d1
gitfebas <64419301+gitfebas@users.noreply.github.com>
2024-03-18 15:14:44 +0000

    Alteração para testar a Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Nada de especial por aqui, mas em breve, construiremos coisas incrveis.</h1>
+    return <h1>Nada de especial por aqui, mas em breve, construiremos coisas incrveis.:-)</h1>
 }
 
 export default Home;
