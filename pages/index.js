5f81ae3aa5f1a46906bbc16cf77e65e6a8925388
gitfebas <64419301+gitfebas@users.noreply.github.com>
2024-03-25 13:28:37 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Nada de especial por aqui, mas em breve, construiremos coisas incrveis.:-)</h1>
+function Home() {
+  return (
+    <h1>
+      Nada de especial por aqui, mas em breve, construiremos coisas incrveis.:-)
+    </h1>
+  );
 }
 
 export default Home;
